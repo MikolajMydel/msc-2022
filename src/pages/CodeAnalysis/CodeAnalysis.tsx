5b40a582e2f01676a0dd60a2b4b2e9d49209024b
Mikolaj Mydel <mydelmikolaj@gmail.com>
2022-12-10 14:44:23 +0100

    Code input page

--- src/pages/CodeAnalysis/CodeAnalysis.tsx
+++ src/pages/CodeAnalysis/CodeAnalysis.tsx
@@ -1,3 +1,9 @@
+// Retrieve code from URL
+// Translate DNA to mRNA (only if code includes T)
+
+// Divide nitrogen bases using 3 different offsets (0, 1, 2)
+// Generate amino acids
+
 import { useParams } from "react-router-dom";
 export default function CodeAnalysis() {
 	const { sequence } = useParams();
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,12 +1,17 @@
 import { RouteObject } from "react-router-dom";
 import Root from "./Root";
 import CodeAnalysis from "./CodeAnalysis/CodeAnalysis";
+import CodeInput from "./CodeInput/CodeInput";
 
 export const routes: RouteObject[] = [
 	{
 		path: "/",
 		element: <Root />,
 		children: [
+			{
+				path: "/",
+				element: <CodeInput />,
+			},
 			{
 				path: "/:sequence",
 				element: <CodeAnalysis />,
