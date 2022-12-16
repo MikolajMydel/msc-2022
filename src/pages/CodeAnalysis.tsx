a6585dbe25965633e3c6daa686fe61f6374b3b86
Pawel Kromka <pawel.kromka@proton.me>
2022-12-16 13:04:42 +0100

    Basic input validation and DNA to RNA translation

--- src/pages/CodeAnalysis.tsx
+++ src/pages/CodeAnalysis.tsx
@@ -1,7 +1,39 @@
 import { useParams } from "react-router-dom";
+
+const DNA_REGEXP = /^[AGCT]+$/;
+const RNA_REGEXP = /^[AGCU]+$/;
+
+
+
+enum InputType {
+	Invalid = 0,
+	RNA,
+	DNA,
+}
+
+function CheckInputType(sequence: string): InputType {	
+	if (DNA_REGEXP.test(sequence)) return InputType.DNA;
+	else if (RNA_REGEXP.test(sequence)) return InputType.RNA;
+	return InputType.Invalid
+}
+
+function GetRNA(input: string | undefined): string {
+	if (!input) return "";
+
+	const inputType = CheckInputType(input);
+
+	if (inputType == InputType.RNA) { return input; }
+	if (inputType == InputType.DNA) { return input.replaceAll('T', 'U'); }
+	
+	return ""; 
+}
+
 export default function CodeAnalysis() {
-	const { sequence } = useParams();
-	return <div>{sequence}</div>;
+	const { sequence } = useParams<{ sequence: string }>();
+	const rna = GetRNA(sequence);
+
+	if (!rna) return <div> Invalid input </div>;
+	return <div> mRNA: {rna} </div>;
 }
 
 // Retrieve code from URL
