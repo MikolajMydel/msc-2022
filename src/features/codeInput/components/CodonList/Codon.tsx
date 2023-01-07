6282fc68d91781319e7f3ba59adc389d36bc8159
Mikolaj Mydel <mydelmikolaj@gmail.com>
2023-01-07 18:08:02 +0100

    Dynamic CSS classes

--- src/features/codeInput/components/CodonList/Codon.tsx
+++ src/features/codeInput/components/CodonList/Codon.tsx
@@ -1,15 +1,24 @@
 import styles from "./Codon.module.scss";
 import classNames from "classnames";
 import { translateCodon } from "../../../../utils/codonOperations";
+import { AminoAcid } from "../../../../utils/staticvalues";
 
 type CodonProps = {
 	nitrogenBases: string;
 };
 export default function Codon({ nitrogenBases }: CodonProps) {
 	const name = translateCodon(nitrogenBases);
+
 	return (
 		<div className={styles.Wrapper}>
-			<div className={classNames(styles.Codon)}>{nitrogenBases}</div>
+			<div
+				className={classNames(styles.Codon, {
+					[styles.CodonStart]: name === AminoAcid.START,
+					[styles.CodonStop]: name === AminoAcid.STOP,
+				})}
+			>
+				{nitrogenBases}
+			</div>
 			<div className={styles.Name}>{name}</div>
 		</div>
 	);
