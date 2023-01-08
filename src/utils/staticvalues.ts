77c74b4d30c59eda3348626f5cb45f7e1749f94e
pk8868 <pawel.kromka@proton.me>
2023-01-08 21:58:28 +0100

    Add atom masses and empty atom numbers

--- src/utils/staticvalues.ts
+++ src/utils/staticvalues.ts
@@ -134,6 +134,7 @@ export interface Atoms {
 
 interface AcidProps {
 	Mass: number;
+	Atoms: Atoms;
 }
 
 interface AcidValues {
@@ -149,88 +150,292 @@ type AcidProperties = {
 export const AMINO_ACID_PROPERTIES: AcidProperties = {
 	Phenylalanine: {
 		Names: { Symbol: "F", Short: "Phe" },
-		Props: { Mass: 147.17386 },
+		Props: {
+			Mass: 147.17386,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Leucine: {
 		Names: { Symbol: "L", Short: "Leu" },
-		Props: { Mass: 113.15764 },
+		Props: {
+			Mass: 113.15764,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Isoleucine: {
 		Names: { Symbol: "I", Short: "Ile" },
-		Props: { Mass: 113.15764 },
+		Props: {
+			Mass: 113.15764,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Methionine: {
 		Names: { Symbol: "M", Short: "Met" },
-		Props: { Mass: 131.19606 },
+		Props: {
+			Mass: 131.19606,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Valine: {
 		Names: { Symbol: "V", Short: "Val" },
-		Props: { Mass: 99.13106 },
+		Props: {
+			Mass: 99.13106,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Serine: {
 		Names: { Symbol: "S", Short: "Ser" },
-		Props: { Mass: 87.0773 },
+		Props: {
+			Mass: 87.0773,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Proline: {
 		Names: { Symbol: "P", Short: "Pro" },
-		Props: { Mass: 97.11518 },
+		Props: {
+			Mass: 97.11518,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Threonine: {
 		Names: { Symbol: "T", Short: "Thr" },
-		Props: { Mass: 101.10388 },
+		Props: {
+			Mass: 101.10388,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Alanine: {
 		Names: { Symbol: "A", Short: "Ala" },
-		Props: { Mass: 71.0779 },
+		Props: {
+			Mass: 71.0779,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Tyrosine: {
 		Names: { Symbol: "Y", Short: "Tyr" },
-		Props: { Mass: 163.17326 },
+		Props: {
+			Mass: 163.17326,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	STOP: {
 		Names: { Symbol: "-", Short: "(STOP)" },
-		Props: { Mass: 0 },
+		Props: {
+			Mass: 0,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Histidine: {
 		Names: { Symbol: "H", Short: "His" },
-		Props: { Mass: 137.13928 },
+		Props: {
+			Mass: 137.13928,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Glutamine: {
 		Names: { Symbol: "Q", Short: "Gln" },
-		Props: { Mass: 128.12922 },
+		Props: {
+			Mass: 128.12922,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Aspargine: {
 		Names: { Symbol: "N", Short: "Asn" },
-		Props: { Mass: 114.10264 },
+		Props: {
+			Mass: 114.10264,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Lysine: {
 		Names: { Symbol: "K", Short: "Lys" },
-		Props: { Mass: 128.17228 },
+		Props: {
+			Mass: 128.17228,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	AsparticAcid: {
 		Names: { Symbol: "D", Short: "Asp" },
-		Props: { Mass: 115.0874 },
+		Props: {
+			Mass: 115.0874,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	GlutamicAcid: {
 		Names: { Symbol: "E", Short: "Glu" },
-		Props: { Mass: 129.11398 },
+		Props: {
+			Mass: 129.11398,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Cysteine: {
 		Names: { Symbol: "C", Short: "Cys" },
-		Props: { Mass: 103.1429 },
+		Props: {
+			Mass: 103.1429,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Tryptophan: {
 		Names: { Symbol: "W", Short: "Trp" },
-		Props: { Mass: 186.2099 },
+		Props: {
+			Mass: 186.2099,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Arginine: {
 		Names: { Symbol: "R", Short: "Arg" },
-		Props: { Mass: 156.18568 },
+		Props: {
+			Mass: 156.18568,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 	Glycine: {
 		Names: { Symbol: "G", Short: "Gly" },
-		Props: { Mass: 57.05132 },
+		Props: {
+			Mass: 57.05132,
+			Atoms: {
+				Hydrogen: 0,
+				Carbon: 0,
+				Oxygen: 0,
+				Nitrogen: 0,
+				Sulphur: 0,
+			},
+		},
 	},
 };
 
 export const WATER_MASS = 18.015;
+export const WATER_ATOMS: Atoms = {
+	Hydrogen: 2,
+	Oxygen: 1,
+	Carbon: 0,
+	Nitrogen: 0,
+	Sulphur: 0,
+};
+
+export const ATOM_MASS: Atoms = {
+	Hydrogen: 1.008,
+	Carbon: 12.01,
+	Oxygen: 15.999,
+	Nitrogen: 14.006,
+	Sulphur: 32.065,
+};
