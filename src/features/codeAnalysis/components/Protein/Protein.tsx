bf5c8ab2823d9a39ffbc47c9bc0fe768b2c4c6eb
pk8868 <pawel.kromka@proton.me>
2023-01-15 13:00:04 +0100

    add highlighting for stable and unstable proteins, display peptide/protein depending on length

--- src/features/codeAnalysis/components/Protein/Protein.tsx
+++ src/features/codeAnalysis/components/Protein/Protein.tsx
@@ -2,6 +2,7 @@ import { getProteinLink } from "../../../../utils/utils";
 import { Protein } from "../../utils/protein";
 import { AminoAcids } from "../AminoAcid/AminoAcid";
 import { ProteinProperties, ProteinPosition } from "./ProteinProperties";
+import styles from "./proteins.module.scss";
 
 type ProteinLinkProps = {
 	protein: Protein;
@@ -24,10 +25,41 @@ type ProteinComponentProps = {
 };
 
 export function ProteinComponent({ protein }: ProteinComponentProps) {
+	const Stable = () => {
+		if (!protein.instabilityIndex) return null;
+		if (protein.instabilityIndex > 40)
+			return (
+				<>
+					, <span className={styles.stable}>stable</span>
+				</>
+			);
+
+		return (
+			<>
+				, <span className={styles.unstable}>unstable</span>
+			</>
+		);
+	};
+	const Name = () => {
+		if (protein.length < 30)
+			return (
+				<h5>
+					<span>Peptide</span>
+					<Stable />
+				</h5>
+			);
+		return (
+			<h4>
+				<span>Protein</span>
+				<Stable />
+			</h4>
+		);
+	};
+
 	return (
 		<div>
 			<ProteinLink protein={protein} />
-			<h5>Protein: </h5>
+			<Name />
 			<h5>Amino acids: </h5>
 			<AminoAcids acids={protein.acids} />
 			<h5>Properties: </h5>
