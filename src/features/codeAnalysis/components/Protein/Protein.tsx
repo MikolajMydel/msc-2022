28e2c7142b6a9cc8ff181f2ff272e6d98faff20d
pk8868 <pawel.kromka@proton.me>
2023-01-16 09:37:03 +0100

    protein component code clean-up

--- src/features/codeAnalysis/components/Protein/Protein.tsx
+++ src/features/codeAnalysis/components/Protein/Protein.tsx
@@ -4,22 +4,6 @@ import { AminoAcids } from "../AminoAcid/AminoAcid";
 import { ProteinProperties, ProteinPosition } from "./ProteinProperties";
 import styles from "./proteins.module.scss";
 
-type ProteinLinkProps = {
-	protein: Protein;
-};
-
-function ProteinLink({ protein }: ProteinLinkProps) {
-	return (
-		<a
-			id={getProteinLink(
-				protein.metadata.codonIndex,
-				protein.metadata.shift,
-				false
-			)}
-		></a>
-	);
-}
-
 type ProteinComponentProps = {
 	protein: Protein;
 };
@@ -27,16 +11,14 @@ type ProteinComponentProps = {
 export function ProteinComponent({ protein }: ProteinComponentProps) {
 	const Stable = () => {
 		if (protein.stable == undefined) return null;
-		if (protein.stable)
-			return (
-				<>
-					, <span className={styles.stable}>stable</span>
-				</>
-			);
-
 		return (
 			<>
-				, <span className={styles.unstable}>unstable</span>
+				,{" "}
+				{protein.stable ? (
+					<span className={styles.stable}>stable</span>
+				) : (
+					<span className={styles.unstable}>unstable</span>
+				)}
 			</>
 		);
 	};
@@ -56,9 +38,21 @@ export function ProteinComponent({ protein }: ProteinComponentProps) {
 		);
 	};
 
+	const ProteinLink = () => {
+		return (
+			<a
+				id={getProteinLink(
+					protein.metadata.codonIndex,
+					protein.metadata.shift,
+					false
+				)}
+			></a>
+		);
+	};
+
 	return (
 		<div>
-			<ProteinLink protein={protein} />
+			<ProteinLink />
 			<Name />
 			<h5>Amino acids: </h5>
 			<AminoAcids acids={protein.acids} />
