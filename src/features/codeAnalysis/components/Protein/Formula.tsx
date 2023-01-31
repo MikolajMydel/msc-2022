b04ea38903abe2c5f41fb799cbe624c1b9f1f3e0
isayni <lennysep@protonmail.com>
2023-01-31 03:02:18 +0100

    drawing the base of the chain using svg

--- src/features/codeAnalysis/components/Protein/Formula.tsx
+++ src/features/codeAnalysis/components/Protein/Formula.tsx
@@ -1,4 +1,3 @@
-import styles from "./Formula.module.scss";
 import { AminoAcid, svgs } from "../../../../utils/staticvalues";
 import { useRef, useEffect } from "react";
 
@@ -13,7 +12,7 @@ function draw(
 	image.addEventListener("load", () => ctx.drawImage(image, x, y));
 }
 
-export function Formula({ acids }: { acids: AminoAcid[] }) {
+export function canvasFormula({ acids }: { acids: AminoAcid[] }) {
 	const ref = useRef<HTMLCanvasElement>(null);
 
 	useEffect(() => {
@@ -27,5 +26,77 @@ export function Formula({ acids }: { acids: AminoAcid[] }) {
 			draw(svgs.circle, ctx, 10, 10);
 		}
 	}, []);
-	return <canvas ref={ref} className={styles.Formula} />;
+	return <canvas ref={ref} />;
+}
+
+export function Formula({ acids }: { acids: AminoAcid[] }) {
+	const properties = {
+		chainLineWidth: 20,
+		chainLineHeight: 15,
+	};
+	const styles = {
+		svg: {
+			backgroundColor: "#fff",
+			height: 120,
+			width: 100 + acids.length * properties.chainLineWidth * 3,
+		},
+		stroke: { fill: "none", stroke: "black", strokeWidth: "2" },
+	};
+	const makeChain = () => {
+		const ns = []; // Nitrogen symbols
+		let x = 30 + properties.chainLineWidth;
+		let y = properties.chainLineHeight;
+		let path = `M ${x} ${y}`;
+		for (let i = 0; i < acids.length; i++) {
+			for (let j = 1; j <= 3; j++) {
+				x += properties.chainLineWidth;
+				y = y == 0 ? properties.chainLineHeight : 0;
+				path += `L ${x} ${y} `;
+				if (j == 2) {
+					ns.push(
+						<text x={x - 5} y={y == 0 ? y - 4 : y + 16}>
+							N
+						</text>
+					);
+				}
+			}
+		}
+		return (
+			<g
+				transform={`translate(${12} ${
+					styles.svg.height / 2 - properties.chainLineHeight / 2
+				})`}
+			>
+				( opening H2N )
+				<text x="0" y="5">
+					<tspan>H</tspan>
+					<tspan dy="5">2</tspan>
+					<tspan dy="-5">N</tspan>
+				</text>
+				( first Carbon )
+				<path
+					style={styles.stroke}
+					d={`M 30 0 L ${30 + properties.chainLineWidth} ${
+						properties.chainLineHeight
+					}`}
+				/>
+				( Oxygen at the end )
+				<text
+					x="0"
+					y="5"
+					transform={`translate(${styles.svg.width - 36} ${0})`}
+				>
+					<tspan>O</tspan>
+				</text>
+				( the path between )
+				<path style={styles.stroke} d={path} />
+				{ns}
+			</g>
+		);
+	};
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" style={styles.svg}>
+			{makeChain()}
+		</svg>
+	);
 }
