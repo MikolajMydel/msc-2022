1189a475a9771eb10160cfc1730d9fd47212ce14
KomarK0X <tomek.jarosz@spoko.pl>
2023-02-11 20:45:05 +0100

    nav-added, button to check analyse-added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,21 @@ import styles from "./Header.module.scss";
 export function Header() {
 	return (
 		<div className={styles.HeaderClass}>
-			<img src="../../../../../img/trophy.png" alt="" />
-			<p>Motorola Science Cup 2022</p>
+			<a href="/">
+				<img src="../../../../../img/trophy.png" alt="" />
+				<p>Motorola Science Cup 2022</p>
+			</a>
+			<div className={styles.Nav}>
+				<a href="/">
+					<p>Home</p>
+				</a>
+				<a href="/about">
+					<p>About</p>
+				</a>
+				<a href="https://github.com/MikolajMydel/msc-2022">
+					<p>GitHub</p>
+				</a>
+			</div>
 		</div>
 	);
 }
--- src/pages/CodeInput/CodeInput.tsx
+++ src/pages/CodeInput/CodeInput.tsx
@@ -8,6 +8,9 @@ export default function CodeInput() {
 			<Header></Header>
 			<div className={styles.InputContainer}>
 				<InputContainer />
+				<a href="/analysis/">
+					<button className={styles.Button}>Check Analyse</button>
+				</a>
 			</div>
 			<FileUploadContainer />
 		</div>
