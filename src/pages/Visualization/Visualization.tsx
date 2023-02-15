3c668654ae2fb15a334a8acbdbe1ada2aba3729a
pk8868 <pawel.kromka@proton.me>
2023-02-15 16:24:42 +0100

    remove visualization page

--- src/pages/Visualization/Visualization.tsx
+++
@@ -1,5 +0,0 @@
-import { useParams } from "react-router";
-export default function Visualization() {
-	const { sequence } = useParams();
-	return <div>{sequence}</div>;
-}
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -3,7 +3,6 @@ import { RouteObject } from "react-router-dom";
 import Root from "./Root/Root";
 import CodeAnalysis from "./CodeAnalysis/CodeAnalysis";
 import CodeInput from "./CodeInput/CodeInput";
-import Visualization from "./Visualization/Visualization";
 
 export const routes: RouteObject[] = [
 	{
@@ -17,10 +16,6 @@ export const routes: RouteObject[] = [
 			{
 				path: "/analysis/:sequence",
 				element: <CodeAnalysis />,
-			},
-			{
-				path: "/visualization/:sequence",
-				element: <Visualization />,
 			},
 		],
 	},
