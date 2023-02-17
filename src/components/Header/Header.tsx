7d60dc9a990a94e639acf5d21bb7baa374de7b1e
KomarK0X <tomek.jarosz@spoko.pl>
2023-02-17 22:27:59 +0100

    media

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,26 +2,33 @@ import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
 export function Header() {
 	return (
-		<div className={styles.HeaderClass}>
-			<Link to={"/"}>
-				<img src="../../../../../img/trophy.png" alt="" />
-				<p>Motorola Science Cup 2022</p>
-			</Link>
-			<div className={styles.Nav}>
+		<div>
+			<div className={styles.HeaderClass}>
 				<Link to={"/"}>
-					<p>Home</p>
+					<img src="../../../../../img/trophy.png" alt="" />
+					<p>Motorola Science Cup 2022</p>
 				</Link>
-				<Link to={"/about"}>
-					<p>About</p>
-				</Link>
-				<a
-					href="https://github.com/MikolajMydel/msc-2022"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<p>GitHub</p>
-				</a>
+				<div className={styles.Nav}>
+					<Link to={"/"}>
+						<p>Home</p>
+					</Link>
+					<Link to={"/about"}>
+						<p>About</p>
+					</Link>
+					<a
+						href="https://github.com/MikolajMydel/msc-2022"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<p>GitHub</p>
+					</a>
+				</div>
 			</div>
+			<img
+				className={styles.Hamburger}
+				src="../../../../../img/menu.png"
+				alt=""
+			/>
 		</div>
 	);
 }
