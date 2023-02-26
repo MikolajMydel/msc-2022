c8ca2a07fcdbf6833808c0734d73f6badb470c35
Mikolaj Mydel <mydelmikolaj@gmail.com>
2023-02-26 11:20:59 +0100

    Frames as table

--- src/features/codeAnalysis/components/Frames/Frame.tsx
+++ src/features/codeAnalysis/components/Frames/Frame.tsx
@@ -2,6 +2,8 @@ import { AminoAcid } from "../../../../utils/staticvalues";
 import { aminoAcidArrayToSections } from "../../../../utils/stringoperations";
 import { v4 as uuid } from "uuid";
 import { Section } from "./Section";
+import styles from "./Frame.module.scss";
+import classNames from "classnames";
 
 type FrameProps = {
 	acids: AminoAcid[];
@@ -12,9 +14,9 @@ type FrameProps = {
 export function Frame({ acids, shift }: FrameProps) {
 	const sections = aminoAcidArrayToSections(acids);
 	return (
-		<div>
-			<span> +{shift} </span>
-			<span>
+		<tr className={styles.TableRow}>
+			<td className={classNames(styles.TableColumn, styles.Shift)}>+{shift}</td>
+			<td className={styles.TableColumn}>
 				{sections.map((section) => (
 					<Section
 						key={uuid()}
@@ -24,8 +26,8 @@ export function Frame({ acids, shift }: FrameProps) {
 						frame={shift}
 					/>
 				))}
-			</span>
-		</div>
+			</td>
+		</tr>
 	);
 }
 
@@ -35,10 +37,15 @@ type ReadingFramesProps = {
 
 export function ReadingFrames({ frames }: ReadingFramesProps) {
 	return (
-		<>
+		<table className={styles.Table}>
+			<thead className={styles.TableHeading}>
+				<td className={styles.TableColumn}>Shift</td>
+				<td className={styles.TableColumn}>Sequence</td>
+			</thead>
+
 			{frames.map((frame, index) => (
 				<Frame key={uuid()} acids={frame} shift={index} />
 			))}
-		</>
+		</table>
 	);
 }
