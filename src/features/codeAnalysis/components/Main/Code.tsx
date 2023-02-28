1ade8f71d98d47d471ae7cfbd1c43a425083eb9e
Mikolaj Mydel <mydelmikolaj@gmail.com>
2023-02-28 17:31:22 +0100

    Display on click

--- src/features/codeAnalysis/components/Main/Code.tsx
+++ src/features/codeAnalysis/components/Main/Code.tsx
@@ -2,17 +2,30 @@ import { AminoAcid } from "../../../../utils/staticvalues";
 import { ReadingFrames } from "../Frames/Frame";
 import styles from "./code.module.scss";
 import classNames from "classnames";
+import { useState } from "react";
+
 type CodeAndFramesParams = {
 	rna: string;
 	frames: AminoAcid[][];
 };
 
 export function CodeAndFrames({ rna, frames }: CodeAndFramesParams) {
+	function switchVisibility() {
+		changeVisibility((isVisible) => !isVisible);
+	}
+	const [isVisible, changeVisibility] = useState<boolean>(false);
 	return (
 		<div className={styles.Wrapper}>
 			<div>
 				<h1 className={styles.MiniTitle}> mRNA:</h1>
-				<div className={classNames(styles.Table, styles.Wrap)}>{rna}</div>
+				<div
+					className={classNames(styles.Table, styles.Wrap, {
+						[styles.WrapVisible]: isVisible,
+					})}
+					onClick={() => switchVisibility()}
+				>
+					{rna}
+				</div>
 			</div>
 			<div>
 				<h1 className={styles.MiniTitle}> Reading frames: </h1>
